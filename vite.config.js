8aedea00d3ef7bbd8c373ca525011aa190eeed08
ShaikhZaamir04 <shaikhzaamir04@gmail.com>
2025-06-16 19:14:24 +0530

    optimized

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    compression(), // Enable gzip compression
+    visualizer({ open: false }) // Set to true if you want to open stats report after build
+  ],
+  resolve: {
+    alias: {
+      '@': '/src', // Cleaner import paths: import x from '@/components/X'
+    },
+  },
+  build: {
+    target: 'es2020', // Better tree-shaking and smaller bundle
+    minify: 'esbuild', // Fastest and most effective for modern projects
+    cssCodeSplit: true,
+    assetsInlineLimit: 8192,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Separate large libs
+        },
+      },
+    },
+  },
+  server: {
+    open: true, // Opens browser on localhost during dev
+  },
+});
